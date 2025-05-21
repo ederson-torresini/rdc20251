bd22014a28ddc34a9876dd336568c691a6716a54
Ederson Torresini <etorresini@ifsc.edu.br>
2025-05-21 18:23:13 +0000

    Adicionar segunda cena

--- src/scenes/Boot.ts
+++ src/scenes/Boot.ts
@@ -1,15 +1,21 @@
-import { Scene } from 'phaser';
+import { Scene } from "phaser";
 
 export class Boot extends Scene {
   constructor() {
-    super({ key: 'Boot' });
+    super({ key: "Boot" });
   }
 
   preload() {
-    this.load.image('corridor', 'assets/corridor.png');
+    this.load.image("corridor", "assets/corridor.png");
   }
 
   create() {
-    this.add.image(400, 225, 'corridor');
+    this.add
+      .image(400, 225, "corridor")
+      .setInteractive()
+      .on("pointerdown", () => {
+        this.scene.stop();
+        this.scene.start("Preloader");
+      });
   }
 }
